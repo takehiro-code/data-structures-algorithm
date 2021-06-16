97b8cb08cfc53d80ce8c226a079084e0e5f0b3d2
Takehiro <narutonaruto717@gmail.com>
2021-06-16 00:32:33 -0700

    Completed assignment 2.

--- assignment2/QueueT.h
+++ assignment2/QueueT.h
@@ -6,18 +6,20 @@ using std::cout;
 using std::endl;
 using std::runtime_error;
 
+template <class T>
 class NodeT
 {
 public:
-	int data;
+	T data;
 	NodeT* next; // pointer that points to the next NodeT
 
 	// Constructors
-	NodeT(int val) : data(val), next(nullptr) { ; };
-	NodeT(int val, NodeT* nd) : data(val), next(nd) { ; };
+	NodeT(T val) : data(val), next(nullptr) { ; };
+	NodeT(T val, NodeT* nd) : data(val), next(nd) { ; };
 };
 
 
+template <class T>
 class QueueT
 {
 public:
@@ -29,7 +31,7 @@ public:
 	// copy constructor
 	// PRE: calling object not initialized
 	// POST: Copying the parameter qt into the calling objec
-	QueueT(const QueueT& qt);
+	QueueT(const QueueT<T>& qt);
 
 	// destructor - deallocates the dynamic memory in the queue
 	~QueueT();
@@ -42,14 +44,12 @@ public:
 	// Insert an element to the back of the queue
 	// PRE: Quene could be either empty or containing existing elements
 	// POST: Parameter val will be inserted to the back of the queue
-	void enqueue(int val);
+	void enqueue(T val);
 
 	// Removes an element from the front of the queue.
 	// PRE: Quene could be either empty or containing existing elements
 	// POST: Empty case: Throws a runtime_error, Non-empty case: remove and returns the front item
-	int dequeue();
-
-
+	T dequeue();
 
 	// prints the elements of the queue, one value per line, from front to back
 	void print() const;
@@ -73,16 +73,16 @@ public:
 	QueueT merge(const QueueT& qt) const;
 
 	// return the front pointer for testing
-	NodeT* getFront() { return front; };
+	NodeT<T>* getFront() { return front; };
 
 private:
-	NodeT* front; // pointer that points to the front node of the queue
-	NodeT* back; // pointer that points to the back node of the queue
+	NodeT<T>* front; // pointer that points to the front node of the queue
+	NodeT<T>* back; // pointer that points to the back node of the queue
 	int sz; // number of items in the queue
 
 	// helper function
-	void copyQueueT(const QueueT& qt);
-	void deleteQueueT();
+	void copyQueueT(const QueueT& qt); // copy the parameter queue into the calling object
+	void deleteQueueT(); // delete the calling object of own queue
 };
 
 
@@ -94,7 +94,8 @@ private:
 // default constructor
 // PRE: calling object not initialized
 // POST: creates empty QueueT
-QueueT::QueueT()
+template <class T>
+QueueT<T>::QueueT()
 {
 	front = nullptr;
 	back = nullptr;
@@ -105,14 +106,16 @@ QueueT::QueueT()
 // copy constructor
 // PRE: calling object not initialized
 // POST: Copying the parameter qt into the calling object
-QueueT::QueueT(const QueueT& qt)
+template <class T>
+QueueT<T>::QueueT(const QueueT<T>& qt)
 {
 	copyQueueT(qt);
 }
 
 
 // destructor - deallocates the dynamic memory in the queue
-QueueT::~QueueT()
+template <class T>
+QueueT<T>::~QueueT()
 {
 	deleteQueueT();
 }
@@ -120,7 +123,8 @@ QueueT::~QueueT()
 // overloaded assignment operator
 // PRE: calling object could be either not initialized or already containing the items
 // POST: The calling object will have copied contents from the passing parameter of queue
-QueueT& QueueT::operator=(const QueueT& qt) 
+template <class T>
+QueueT<T>& QueueT<T>::operator=(const QueueT<T>& qt) 
 {
 	if (this != &qt) {
 		// need to delete to clean up the existing contents before copying
@@ -134,16 +138,17 @@ QueueT& QueueT::operator=(const QueueT& qt)
 // Insert an element to the back of the queue
 // PRE: Quene could be either empty or containing existing elements
 // POST: Parameter val will be inserted to the back of the queue
-void QueueT::enqueue(int val)
+template <class T>
+void QueueT<T>::enqueue(T val)
 {
 	// if the queue is empty, insert an element and let front and back point to this node
 	if (sz == 0) {
-		NodeT* newNodeT = new NodeT(val);
+		NodeT<T>* newNodeT = new NodeT<T>(val);
 		front = newNodeT;
 		back = newNodeT;
 	}
 	else {
-		NodeT* newNodeT = new NodeT(val);
+		NodeT<T>* newNodeT = new NodeT<T>(val);
 		back->next = newNodeT; // update previous node to point from null to the newly created node
 		back = newNodeT; // update back pointer to point from previous node to the newly created node
 	}
@@ -154,14 +159,15 @@ void QueueT::enqueue(int val)
 // Removes an element from the front of the queue.
 // PRE: Quene could be either empty or containing existing elements
 // POST: Empty case: Throws a runtime_error, Non-empty case: remove and returns the front item
-int QueueT::dequeue()
+template <class T>
+T QueueT<T>::dequeue()
 {
 	if (sz == 0) {
 		throw runtime_error("Attempt of removing an item from the empty queue");
 	}
 	else {
-		int removedItem = front->data;
-		NodeT* nextNode = front->next;
+		T removedItem = front->data;
+		NodeT<T>* nextNode = front->next;
 		delete front;
 		front = nextNode;
 		sz--;
@@ -171,9 +177,10 @@ int QueueT::dequeue()
 
 
 // prints the elements of the queue, one value per line, from front to back
-void QueueT::print() const
+template <class T>
+void QueueT<T>::print() const
 {
-	NodeT* p = front; // pointer that points to the current position of the node in queue
+	NodeT<T>* p = front; // pointer that points to the current position of the node in queue
 	while (p != nullptr) {
 		cout << p->data << endl;
 		p = p->next;
@@ -182,7 +189,8 @@ void QueueT::print() const
 
 
 // check if queue is empty or not
-bool QueueT::empty() const
+template <class T>
+bool QueueT<T>::empty() const
 {
 	if (sz == 0) {
 		return true;
@@ -194,7 +202,8 @@ bool QueueT::empty() const
 
 
 // returns the number of elements stored in the queue
-int QueueT::size() const
+template <class T>
+int QueueT<T>::size() const
 {
 	return sz;
 }
@@ -204,7 +213,8 @@ int QueueT::size() const
 // PRE: Calling object is initialized already but could be either empty or non-empty
 // PARAM: Passing parameter queue and its integer n, the first n values to take and concatenate to the calling object 
 // POST: First n values of parameter of the queue will be removed and added to the calling object
-void QueueT::concatenate(QueueT& qt, int n)
+template <class T>
+void QueueT<T>::concatenate(QueueT<T>& qt, int n)
 {
 	if (n < 0 || n > qt.size()) {
 		throw runtime_error("Number of values exceeds the queue size");
@@ -213,7 +223,7 @@ void QueueT::concatenate(QueueT& qt, int n)
 		for (int i = 0; i < n; i++) 
 		{
 			// remove the item from qt at the front and add it to the back of the calling object queue
-			int removedItem = qt.dequeue();
+			T removedItem = qt.dequeue();
 			enqueue(removedItem);
 		}
 	}
@@ -224,13 +234,14 @@ void QueueT::concatenate(QueueT& qt, int n)
 // PRE: Given two queues (calling object and parameter)
 // PARAM: Constant reference parameter of queue
 // POST: Returns a merged queue. e.g. calling object = {a,b,c,d,e}, parameter = {r,s,t}, result = {a,r,b,s,c,t,d,e}
-QueueT QueueT::merge(const QueueT& qt) const
+template <class T>
+QueueT<T> QueueT<T>::merge(const QueueT<T>& qt) const
 {
-	QueueT result;
+	QueueT<T> result;
 
 	// position pointers for each queue starting from front node
-	NodeT* p_calling = front;
-	NodeT* p_param = qt.front;
+	NodeT<T>* p_calling = front;
+	NodeT<T>* p_param = qt.front;
 
 	// if both of the queues reach nullptr, go out of the loop
 	while (p_calling != nullptr || p_param != nullptr) 
@@ -256,7 +267,8 @@ QueueT QueueT::merge(const QueueT& qt) const
 // --------------------------------------------------------------------------------------
 
 // helper function used in the copy constructor and overloaded assignment operator
-void QueueT::copyQueueT(const QueueT& qt)
+template <class T>
+void QueueT<T>::copyQueueT(const QueueT<T>& qt)
 {
 	// initialize the front and back pointers
 	sz = 0;
@@ -264,7 +276,7 @@ void QueueT::copyQueueT(const QueueT& qt)
 	back = nullptr;
 
 	// copy the contents to the calling object from front to back
-	NodeT* p = qt.front; // position pointer starting from front to back
+	NodeT<T>* p = qt.front; // position pointer starting from front to back
 	while (p != nullptr)
 	{
 		enqueue(p->data);
@@ -274,12 +286,13 @@ void QueueT::copyQueueT(const QueueT& qt)
 
 
 // helper function used in the overloaded assignment operator and destructor
-void QueueT::deleteQueueT()
+template <class T>
+void QueueT<T>::deleteQueueT()
 {
 	// de-allocates the memory
 	while (front != nullptr)
 	{
-		NodeT* nextNode = front->next;
+		NodeT<T>* nextNode = front->next;
 		delete front;
 		front = nextNode;
 	}
--- assignment2/README.md
+++ assignment2/README.md
@@ -1,6 +1,6 @@
 # Assignment 2
 
-Template class of Queue implementation using a singly LinkedList. Since we are implementing a template class, we include the class declaration and its definition inside of single header file.
+Template class of Queue implementation using a singly LinkedList. Since we are implementing a template class, we include the class declaration and its member function definition inside of single header file and outside of its class declaration.
 
 
 ## Files
@@ -36,3 +36,12 @@ Print: Printing from "front" to "back"
 
 Result is 7, 5, 3, 1
 
+Do you need `inline`? -> no
+
+Specifying a method defined outside the class definition as inline, using the keyword, is a hint* to the compiler to compile a function in a way that reduce the amount of overhead in function calling. This is useful for small functions that are called repeatedly
+
+For template class, when to place <...>?
+- Inside of the template class definitions, no need to put <type_parameter>.
+- Put <type_parameter> for other template class object even inside of the class definition.
+- Put <type_parameter> for each method as class_name <type_parameter>:: outside of the class definition.
+- How about the method parameter in our example const List<T>& lst, do we need here or si it optional?
